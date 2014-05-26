43fe8f112d67567104c98d9f1863e9bbd77e3298
Brandon Truong <me@brandontruong.com>
2014-05-25 23:32:30 -0500

    Rough site completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-
+$(document).ready(function(){
+	var menuDown = true;
+	var thin = false;
+	var amt;
+	$('.menuToggle').click(function(){
+		amt = ($(document).width() < 769) ? 260 : 50;
+		if(menuDown == true){
+			$('nav').slideUp(500);
+			$('.menuToggle').animate({marginTop: "0px"}, 500);
+			menuDown = false;
+		}
+		else{
+			$('nav').slideDown(500);
+			$('.menuToggle').animate({marginTop: "+=" + amt.toString() + "px"}, 500);
+			menuDown = true;
+		}
+	})
+});
