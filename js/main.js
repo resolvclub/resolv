c014326ce3097a7fade3ad214767ec0a913340da
Brandon Truong <me@brandontruong.com>
2014-06-30 14:29:02 -0400

    Overhaul on some design stuff

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
 $(document).ready(function(){
 	$('#hello > *').fadeIn(1000);
 	$('.projectPage > *').fadeIn(1000);
-	var menuDown = true;
-	var thin = false;
-	var amt;
+	var menuDown = false;
 	$('.menuToggle').click(function(){
-		amt = ($(document).width() < 769) ? 260 : 50;
 		if(menuDown == true){
-			$('nav').slideUp(500);
-			$('.menuToggle').animate({marginTop: "0px"}, 500);
+			$('nav ul').slideUp(300);
 			menuDown = false;
 		}
 		else{
-			$('nav').slideDown(500);
-			$('.menuToggle').animate({marginTop: "+=" + amt.toString() + "px"}, 500);
+			$('nav ul').slideDown(300);
 			menuDown = true;
 		}
 	});
