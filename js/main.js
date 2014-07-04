4dbecbad401af64a1b4261945890bfe045a8d3a6
Brandon Truong <me@brandontruong.com>
2014-07-04 16:34:38 -0400

    Removed unnecessary, projects on single page, menu more compatibility

--- js/main.js
+++ js/main.js
@@ -4,24 +4,29 @@ $(document).ready(function(){
 	var menuDown = false;
 	$('.menuToggle').click(function(){
 		if(menuDown == true){
+			// $("nav").height("50px");
 			$('nav ul').slideUp(300);
 			menuDown = false;
 		}
 		else{
+			// $('nav').height("100px");
 			$('nav ul').slideDown(300);
 			menuDown = true;
 		}
 	});
 
 	window.addEventListener("scroll",function() { 
-   if(window.scrollY > $("section#home").height()) {
-      $('nav').animate({backgroundColor: "#ecf0f1"}, {duration: 100});
-   }
-   else {
-      $('nav').animate({backgroundColor: "transparent"}, {duration: 100});
-   }
-   var yPos = $(window).scrollY / 2;
-   $("#home").css("backgroundPosition", yPos);
+		if($(window).width() > 768){
+	    if(window.scrollY > $("section#home").height()) {
+	      $('nav').animate({backgroundColor: "#ecf0f1"}, {duration: 100});
+	    }
+	    else {
+	      $('nav').animate({backgroundColor: "transparent"}, {duration: 100});
+	      $('.menuToggle').animate({backgroundColor: "transparent"}, {duration: 100});
+	    }
+	  } else {
+	  	$('nav').animate({backgroundColor: "#ecf0f1"}, {duration: 100});
+	  }
 	},false);
 
 	$(window).resize(function(){
@@ -32,8 +37,9 @@ $(document).ready(function(){
 	});
 
 	$(".projectToggle").click(function(){
-		if($(this).siblings(".projectContent").css("display") == "none")
+		if($(this).siblings(".projectContent").css("display") == "none"){
 			$(this).siblings(".projectContent").slideDown();
+		}
 		else $(this).siblings(".projectContent").slideUp();
 	});
 
